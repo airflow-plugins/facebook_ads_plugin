0e505cc0885906ec70638ab201535221dc4bf7ab
Ben <ben@astronomer.io>
2018-05-15 13:53:52 -0600

    Update schema.py

--- schemas/schema.py
+++ schemas/schema.py
@@ -1,1 +1,24 @@
-
+facebook_ads = [
+                { 'name': 'account_id', 'type': 'varchar(64)' },
+                { 'name': 'ad_id', 'type': 'varchar(64)' },
+                { 'name': 'adset_id', 'type': 'varchar(64)' },
+                { 'name': 'campaign_id', 'type': 'varchar(64)' },
+                { 'name': 'date_start', 'type': 'date' },
+                { 'name': 'date_stop', 'type': 'date' },
+                { 'name': 'ad_name', 'type': 'varchar(255)' },
+                { 'name': 'adset_name', 'type': 'varchar(255)' },
+                { 'name': 'campaign_name', 'type': 'varchar(255)' },
+                { 'name': 'clicks', 'type': 'int(11)' },
+                { 'name': 'cpc', 'type': 'decimal(20,6)' },
+                { 'name': 'cpm', 'type': 'decimal(20,6)' },
+                { 'name': 'cpp', 'type': 'decimal(20,6)' },
+                { 'name': 'ctr', 'type': 'decimal(20,6)' },
+                { 'name': 'impressions', 'type': 'int(11)' },
+                { 'name': 'objective', 'type': 'varchar(255)' },
+                { 'name': 'reach', 'type': 'int(11)' },
+                { 'name': 'social_clicks', 'type': 'int(11)' },
+                { 'name': 'social_impressions', 'type': 'int(11)' },
+                { 'name': 'social_spend', 'type': 'decimal(20,6)' },
+                { 'name': 'spend', 'type': 'decimal(20,6)' },
+                { 'name': 'total_unique_actions', 'type': 'int(11)' }
+               ]
