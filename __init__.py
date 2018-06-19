a304a55631ce4c582461493f4aac2ae43c74d70a
Ben <ben@astronomer.io>
2018-06-19 10:57:47 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@ from facebook_ads_plugin.hooks.facebook_ads_hook import FacebookAdsHook
 from facebook_ads_plugin.operators.facebook_ads_to_s3_operator import FacebookAdsInsightsToS3Operator
 
 
-class PGFacebookAdsPlugin(AirflowPlugin):
-    name = "PGFacebookAdsPlugin"
+class FacebookAdsPlugin(AirflowPlugin):
+    name = "FacebookAdsPlugin"
     hooks = [FacebookAdsHook]
     operators = [FacebookAdsInsightsToS3Operator]
     executors = []
